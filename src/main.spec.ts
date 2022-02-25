4db0a77c98b72b177e536d6bc2c0b5df2de882cd
mustafa <mustafa@dwekat.com>
2022-02-26 00:33:01 +0400

    test: add unit testing

---
+++ src/main.spec.ts
@@ -0,0 +1,30 @@
+/**
+ *
+ */
+import Stemmer from './main';
+
+describe('Stemmer', () => {
+  let stemmer: Stemmer;
+
+  beforeEach(() => {
+    stemmer = new Stemmer();
+  });
+
+  it('Should stem correctly', () => {
+    const stemmed = stemmer.stem('المستشفيات');
+    expect(stemmed.stem).toEqual(['شفي', 'سشف']);
+    expect(stemmed.normalized).toBe('مستشف');
+  });
+
+  it('Should stem correctly', () => {
+    const stemmed = stemmer.stem('يزرع');
+    expect(stemmed.stem).toEqual(['زرع']);
+    expect(stemmed.normalized).toBe('زرع');
+  });
+
+  it('Should stem correctly', () => {
+    const stemmed = stemmer.stem('السرور');
+    expect(stemmed.stem).toEqual(['سرر']);
+    expect(stemmed.normalized).toBe('سرور');
+  });
+});
